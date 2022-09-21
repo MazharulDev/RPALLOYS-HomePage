d78ac2496a348617533a9a28c9119e4cb935853f
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-21 11:39:56 +0600

    title,favicon and navbar created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { BsArrowRight } from 'react-icons/bs'
+import icono from '../../assets/icon/Vector.png'
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className='flex justify-between items-center mx-4 p-4'>
+            <HiMenuAlt3 className='text-xl text-[#222222]' />
+            <h2 className='text-2xl font-bold flex justify-center items-center text-[#222222]'><span>RP ALL</span> <img className='w-4' src={icono} alt="" /> <span>YS</span> </h2>
+            <div className='flex justify-center items-center gap-2 border-b-2 border-[#222222] text-[#222222]'>
+                <a href="#">Get in touch</a>
+                <BsArrowRight />
+            </div>
         </div>
     );
 };
